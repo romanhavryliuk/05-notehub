59f611d284d3edb9c76c948851a30ff8fcf293ff
Roman Havryliuk <romanfest1221@gmail.com>
2026-03-22 23:55:43 +0100

    feat: integrate delete functionality in NoteList component

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,12 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "../../services/noteService";
 import type { Note } from "../../types/note";
 import css from "./NoteList.module.css";
 
 interface NoteListProps {
   notes: Note[];
-  onDelete: (id: string) => void;
 }
 
-const NoteList = ({ notes, onDelete }: NoteListProps) => {
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (id: string) => deleteNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
   return (
     <ul className={css.list}>
       {notes.map(({ id, title, content, tag }) => (
@@ -15,7 +25,11 @@ const NoteList = ({ notes, onDelete }: NoteListProps) => {
           <p className={css.content}>{content}</p>
           <div className={css.footer}>
             <span className={css.tag}>{tag}</span>
-            <button className={css.button} onClick={() => onDelete(id)}>
+            <button
+              className={css.button}
+              onClick={() => mutate(id)}
+              disabled={isPending}
+            >
               Delete
             </button>
           </div>
@@ -23,6 +37,4 @@ const NoteList = ({ notes, onDelete }: NoteListProps) => {
       ))}
     </ul>
   );
-};
-
-export default NoteList;
+}
